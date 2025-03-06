6b7a251a2a894e0d18b479174d29953b36918c69
Daniel Castaño Rodríguez <UO288241@uniovi.es>
2025-03-06 12:16:51 +0100

    Documentar zint_to_float()

--- t8spectrum/util/decoder.py
+++ t8spectrum/util/decoder.py
@@ -6,5 +6,14 @@ import numpy as np
 
 
 def zint_to_float(raw):
+    """
+    Convert a base64 encoded compressed string of 16-bit integers to a NumPy array of floats.
+
+    Args:
+        raw (str): A base64 encoded string containing compressed 16-bit integer data.
+
+    Returns:
+        np.ndarray: A NumPy array of floats obtained by decompressing and decoding the input string.
+    """
     decompressed_data = decompress(b64decode(raw.encode()))
     return np.array([unpack('h', decompressed_data[i*2:(i+1)*2])[0] for i in range(int(len(decompressed_data)/2))], dtype='f')
