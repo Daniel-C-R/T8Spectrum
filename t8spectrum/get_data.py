80e9a18df4b5a5df3b0b53b7c37d41c9ebb061dc
Daniel Castaño Rodríguez <UO288241@uniovi.es>
2025-03-06 12:06:35 +0100

    Documentar get_waveform().

--- t8spectrum/get_data.py
+++ t8spectrum/get_data.py
@@ -17,6 +17,25 @@ def get_waveform(
         t8_user: str,
         t8_password: str
 ) -> tuple[np.ndarray, int]:
+    """
+    Fetches waveform data from a specified host.
+
+    Args:
+        host (str): The hostname or IP address of the server.
+        id (str): The T8 ID.
+        machine (str): The machine identifier.
+        point (str): The point tag.
+        pmode (str): The processing mode tag.
+        time (datetime | int): The waveform timestamp, either as a datetime object or an integer timestamp.
+        t8_user (str): The username for authentication.
+        t8_password (str): The password for authentication.
+
+    Returns:
+        tuple[np.ndarray, int]: A tuple containing the waveform data as a numpy array and the sample rate as an integer.
+
+    Raises:
+        Exception: If the request to fetch the waveform data fails.
+    """
     if type(time) is datetime:
         time = time.timestamp()
 
