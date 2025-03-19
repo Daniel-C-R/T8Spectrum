5428877a13c4f4f7b669263f79bf954a7077f88c
Daniel Castaño Rodríguez <UO288241@uniovi.es>
2025-03-19 11:10:27 +0100

    Crear función `iso_string_to_timestamp`.
    La función `iso_string_to_timestamp` convierte una cadena de texto en
    formato ISO a un timestamp. Se ha escrito también un test para probarla.

--- t8_client/util/timestamp.py
+++ t8_client/util/timestamp.py
@@ -26,3 +26,16 @@ def timestamp_to_iso_string(timestamp: int) -> str:
         str: The ISO 8601 formatted string representation of the timestamp.
     """
     return datetime.fromtimestamp(timestamp, tz=UTC).strftime("%Y-%m-%dT%H:%M:%S")
+
+
+def iso_string_to_timestamp(iso_string: str) -> int:
+    """
+    Convert an ISO 8601 formatted string to a Unix timestamp.
+
+    Args:
+        iso_string (str): The ISO 8601 formatted string to convert.
+
+    Returns:
+        int: The Unix timestamp representation of the ISO 8601 formatted string.
+    """
+    return int(datetime.fromisoformat(iso_string).replace(tzinfo=UTC).timestamp())
