9dfcd97e0b88e97344a8c210d3a4b71bc381a39a
Daniel Castaño Rodríguez <UO288241@uniovi.es>
2025-03-19 11:02:05 +0100

    Añadir comando para obtener la lista de espectros.
    Se ha añadido un nuevo comando `list-spectra` para obtener la lista de
    espectros para una máquina, punto y modo de procesamiento dados. De
    igual forma que `list-waves`, retorna los datos como una serie de fechas
    a poder consultar.

--- t8_client/cli.py
+++ t8_client/cli.py
@@ -15,14 +15,21 @@ def cli(ctx):
     ctx.obj["T8_PASSWORD"] = os.getenv("T8_PASSWORD")
 
 
+def pmode_params(func):
+    func = click.option("-M", "--machine", required=True, help="Machine tag")(func)
+    func = click.option("-p", "--point", required=True, help="Point tag")(func)
+    func = click.option("-m", "--pmode", required=True, help="Processing mode tag")(
+        func
+    )
+    return func
+
+
 @cli.command(
     name="list-waves",
     help="List all the waves for a given machine, point, and processing mode as a"
     + " list of dates.",
 )
-@click.option("-M", "--machine", required=True, help="Machine tag")
-@click.option("-p", "--point", required=True, help="Point tag")
-@click.option("-m", "--pmode", required=True, help="Processing mode tag")
+@pmode_params
 @click.pass_context
 def list_waves(ctx, machine, point, pmode):
     for wave in get_data.get_wave_list(
@@ -35,6 +42,26 @@ def list_waves(ctx, machine, point, pmode):
         t8_password=ctx.obj["T8_PASSWORD"],
     ):
         print(wave)
+
+
+@cli.command(
+    name="list-spectra",
+    help="List all the spectra for a given machine, point, and processing mode as a"
+    + " list of dates.",
+)
+@pmode_params
+@click.pass_context
+def list_spectra(ctx, machine, point, pmode):
+    for spectra in get_data.get_spectra(
+        host=ctx.obj["HOST"],
+        id=ctx.obj["ID"],
+        machine=machine,
+        point=point,
+        pmode=pmode,
+        t8_user=ctx.obj["T8_USER"],
+        t8_password=ctx.obj["T8_PASSWORD"],
+    ):
+        print(spectra)
 
 
 if __name__ == "__main__":
