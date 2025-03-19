592a7ea869a869318c3e6de75451f035434c1012
Daniel Castaño Rodríguez <UO288241@uniovi.es>
2025-03-19 10:36:57 +0100

    Añadir comando para listar las waveforms.
    Añadir el comando `list-waves` para obtener la lista de formas de onda
    para una máquina, punto y modo de procesamiento dados. Esta lista
    consiste en una serie de fechas que se pueden consultar.

--- t8_client/cli.py
+++ t8_client/cli.py
@@ -1,9 +1,40 @@
+import os
+
 import click
+
+from t8_client import get_data
 
 
 @click.group()
-def cli():
-    pass
+@click.pass_context
+def cli(ctx):
+    ctx.ensure_object(dict)
+    ctx.obj["HOST"] = os.getenv("HOST")
+    ctx.obj["ID"] = os.getenv("ID")
+    ctx.obj["T8_USER"] = os.getenv("T8_USER")
+    ctx.obj["T8_PASSWORD"] = os.getenv("T8_PASSWORD")
+
+
+@cli.command(
+    name="list-waves",
+    help="List all the waves for a given machine, point, and processing mode as a"
+    + " list of dates.",
+)
+@click.option("-M", "--machine", required=True, help="Machine tag")
+@click.option("-p", "--point", required=True, help="Point tag")
+@click.option("-m", "--pmode", required=True, help="Processing mode tag")
+@click.pass_context
+def list_waves(ctx, machine, point, pmode):
+    for wave in get_data.get_wave_list(
+        host=ctx.obj["HOST"],
+        id=ctx.obj["ID"],
+        machine=machine,
+        point=point,
+        pmode=pmode,
+        t8_user=ctx.obj["T8_USER"],
+        t8_password=ctx.obj["T8_PASSWORD"],
+    ):
+        print(wave)
 
 
 if __name__ == "__main__":
